0b2e76131093245edb07d033916170f7c80e49ed
Gustavo <lizarraga.dev@gmail.com>
2019-12-22 22:49:41 -0400

    Final code RecyclerViewSimple

--- AK_Fundamentals_10_RecyclerViewSimple/app/src/main/java/com/miramicodigo/rvsimple/MainActivity.kt
+++ AK_Fundamentals_10_RecyclerViewSimple/app/src/main/java/com/miramicodigo/rvsimple/MainActivity.kt
@@ -2,6 +2,14 @@ package com.miramicodigo.rvsimple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +17,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        rvDatos.layoutManager = LinearLayoutManager(this)
+        val listaDatos = llenarDatos()
+        val adaptador = RecyclerViewAdapter(listaDatos)
+        rvDatos.adapter = adaptador
+    }
+
+    fun llenarDatos(): ArrayList<String> {
+        var datos = ArrayList<String>()
+        for(i in 0..500) {
+            datos.add("Item en posicion ${i + 1}")
+        }
+        return datos
+    }
+
+    inner class RecyclerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var tvTexto = itemView as TextView
+
+        init {
+            itemView.setOnClickListener {
+                Toast.makeText(applicationContext, "Click en ${adapterPosition + 1}", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    inner class RecyclerViewAdapter(val data: ArrayList<String>) : RecyclerView.Adapter<RecyclerViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+            val layoutInflater = LayoutInflater.from(applicationContext)
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+            return RecyclerViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return data.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+            val dato = data[position]
+            holder.tvTexto.text = dato
+        }
 
     }
 
